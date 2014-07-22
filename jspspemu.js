1df53b83904021982954c423139836260ec84a6d
soywiz <soywiz@gmail.com>
2014-07-22 22:59:30 +0200

    - Added share modal

--- jspspemu.js
+++ jspspemu.js
@@ -2125,7 +2125,7 @@ var Pointer = (function () {
 })();
 //# sourceMappingURL=struct.js.map
 
-﻿///<reference path="../../typings/promise/promise.d.ts" />
+///<reference path="../../typings/promise/promise.d.ts" />
 var __extends = this.__extends || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
